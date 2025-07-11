6b97acdf141dfc9522e6c8f1a8a09df9d8f11e81
Shamoon Ahmed <samsonrizzy6@gmail.com>
2025-07-11 16:56:19 +0500

    Fetching data from web using GET method

--- requests/get.py
+++ requests/get.py
@@ -1,43 +1,54 @@
 import requests
 import os
 
-API_KEY = os.getenv("WEATHER_API_KEY")
+# # EXAMPLE 1:
 
-# fetching a user profile so we just pass the url with the user id and get response
-r = requests.get("https://www.linkedin.com/in/shamoon-ahmed-496a6231b/")
+# # fetching a user profile so we just pass the url with the user id and get response
+# r = requests.get("https://www.linkedin.com/in/shamoon-ahmed-496a6231b/")
 
-print("Response: ", r.text)
-print("Status Code: ", r.status_code)
+# print("Response: ", r.text)
+# print("Status Code: ", r.status_code)
 
-# fetching the weather, we pass some parameters. Those parameters are are visible in the url
-# the requests library automatically encodes the query like %20 or + in place of a space ( )
+# # EXAMPLE 2:
 
-city = input("Enter City: ").capitalize()
+# # fetching the weather, we pass some parameters. Those parameters are are visible in the url
+# # the requests library automatically encodes the query like %20 or + in place of a space ( )
+# # not every url gives a json response. instead they give html text response
 
-params = {
-    "q" : city,
-    "appid" : API_KEY
-}
+# API_KEY = os.getenv("WEATHER_API_KEY")
 
-url = f"http://api.openweathermap.org/geo/1.0/direct"
+# city = input("Enter City: ").capitalize()
 
-r = requests.get(url, params=params)
-data = r.json()
-lat = data[0]["lat"]
-lon = data[0]["lon"]
-print("Response -> ", "City: ", data[0]["name"], "| Country: ", data[0]["country"], "| Latitude: ", data[0]["lat"], "| Longitude: ", data[0]["lon"])
+# params = {
+#     "q" : city,
+#     "appid" : API_KEY
+# }
 
-parameters = {
-    "lat" : lat,
-    "lon" : lon,
-    "appid" : API_KEY
-}
+# url = f"http://api.openweathermap.org/geo/1.0/direct"
 
-weather_url = "https://api.openweathermap.org/data/2.5/weather"
+# r = requests.get(url, params=params)
+# data = r.json()
+# lat = data[0]["lat"]
+# lon = data[0]["lon"]
+# print("Response -> ", "City: ", data[0]["name"], "| Country: ", data[0]["country"], "| Latitude: ", data[0]["lat"], "| Longitude: ", data[0]["lon"])
 
-r = requests.get(weather_url, params=parameters)
+# parameters = {
+#     "lat" : lat,
+#     "lon" : lon,
+#     "appid" : API_KEY
+# }
 
-weather_data = r.json()
+# weather_url = "https://api.openweathermap.org/data/2.5/weather"
 
-print("Response -> ", "City: ", data[0]["name"], "| Country: ", data[0]["country"], "| Weather: ", weather_data["weather"][0]["main"], "| Description: ", weather_data["weather"][0]["description"])
+# r = requests.get(weather_url, params=parameters)
 
+# weather_data = r.json()
+
+# print("Response -> ", "City: ", data[0]["name"], "| Country: ", data[0]["country"], "| Weather: ", weather_data["weather"][0]["main"], "| Description: ", weather_data["weather"][0]["description"])
+
+# EXAMPLE 3: 
+url = "https://media.greatbigphotographyworld.com/wp-content/uploads/2014/11/Landscape-Photography-steps.jpg"
+image = requests.get(url)
+
+with open("Landscape-Photography-steps.jpg", "wb") as f:
+    f.write(image.content)
