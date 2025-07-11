0ef9b860d1983708c6092e3f9ca54559e8ec8b84
Shamoon Ahmed <samsonrizzy6@gmail.com>
2025-07-11 16:28:35 +0500

    Fetching weather

--- requests/get.py
+++ requests/get.py
@@ -1,5 +1,40 @@
 import requests
 
+# fetching a user profile so we just pass the url with the user id and get response
 r = requests.get("https://www.linkedin.com/in/shamoon-ahmed-496a6231b/")
-# print(r.text)
-print(r.status_code)
+
+print("Response: ", r.text)
+print("Status Code: ", r.status_code)
+
+# fetching the weather, we pass some parameters that are visible in the url
+# the requests library automatically encodes the query like %20 or + in place of a space ( )
+
+city = input("Enter City: ").capitalize()
+
+params = {
+    "q" : city,
+    "appid" : "85f6422f798147ed37a61ecda17d2bb5"
+}
+
+url = f"http://api.openweathermap.org/geo/1.0/direct"
+
+r = requests.get(url, params=params)
+data = r.json()
+lat = data[0]["lat"]
+lon = data[0]["lon"]
+print("Response -> ", "City: ", data[0]["name"], "| Country: ", data[0]["country"], "| Latitude: ", data[0]["lat"], "| Longitude: ", data[0]["lon"])
+
+parameters = {
+    "lat" : lat,
+    "lon" : lon,
+    "appid" : "85f6422f798147ed37a61ecda17d2bb5"
+}
+
+weather_url = "https://api.openweathermap.org/data/2.5/weather"
+
+r = requests.get(weather_url, params=parameters)
+
+weather_data = r.json()
+
+print("Response -> ", "City: ", data[0]["name"], "| Country: ", data[0]["country"], "| Weather: ", weather_data["weather"][0]["main"], "| Description: ", weather_data["weather"][0]["description"])
+
