9241a8220af0c9b770f2478da4bc711f0b93eb20
Shamoon Ahmed <samsonrizzy6@gmail.com>
2025-07-12 14:43:24 +0500

    Working with POST method

--- requests/post.py
+++ requests/post.py
@@ -1,5 +1,7 @@
 import requests
 
+# EXAMPLE 1: creating a new blog post
+
 url = "https://jsonplaceholder.typicode.com/posts"
 
 payload = {
@@ -10,7 +12,38 @@ payload = {
 }
 
 r = requests.post(url, data=payload)
-print(r.json())
+# print(r.json())
+
+r = requests.delete("https://jsonplaceholder.typicode.com/posts/1")
+
+print(r.status_code)
 
 response = requests.get(url)
-print(response.json())
+# print(response.json())
+
+# EXAMPLE 2: creating a new user account
+
+url = "https://reqres.in/api/register"
+
+headers = {
+    "x-api-key" : "reqres-free-v1"
+}
+payload = {
+    "email" : "eve.holt@reqres.in",
+    "password" : "pistol"
+}
+
+res = requests.post(url, data=payload, headers=headers)
+# print(res.json())
+
+# EXAMPLE 3: Uploading a file 
+
+url = "https://httpbin.org/post"
+
+files = {
+    "file" : ("test.txt", open("test.txt", "rb"))
+}
+
+resp = requests.post(url, files=files)
+print(resp.json())
+print(resp.status_code)
